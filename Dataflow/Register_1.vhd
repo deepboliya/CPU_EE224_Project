b533a0c5b8b40a640a76c6bf96b7f56e76cc7cf0
deepboliya <109439950+deepboliya@users.noreply.github.com>
2022-11-30 23:55:44 +0530

    Update Register_1.vhd

--- Dataflow/Register_1.vhd
+++ Dataflow/Register_1.vhd
@@ -6,24 +6,16 @@ entity Register_1 is
     port (
         DIN : in std_logic;
 		  DOUT : out std_logic;
-        CLK, WE : in std_logic
+        WE : in std_logic
     );
 end entity Register_1;
 
 architecture Register_1_Arch of Register_1 is
+
+    signal stored_DIN : std_logic;
 begin
-    process(CLK, WE, DIN)
-    variable stored_DIN : std_logic;
-    begin
-        if rising_edge(CLK) then
-            if WE = '1' then
-                stored_DIN := DIN;
-            else 
-                null;
-            end if;
-        else 
-            null;
-        end if;
-        DOUT <= stored_DIN after 5ns;
-    end process;
+                stored_DIN <= DIN when (WE='1');
+     
+        DOUT <= stored_DIN ;
+    
 end architecture Register_1_Arch;
